c7bdeaf11a4f727c20ecc9b9a054925fb097aef4
abdelrahman-abied <abied_2011@yahoo.com>
2023-01-28 11:45:02 +0200

    check sms permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
 import '../view/home/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var permission = await Permission.sms.status;
+  if (!permission.isGranted) {
+    await [Permission.sms].request();
+  }
   runApp(
     const ProviderScope(
       child: MyApp(),
